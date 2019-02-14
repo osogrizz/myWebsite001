0975bc928265f87c9021342d9ec74784141ec92b
osogrizz <lhtor@gmail.com>
2019-02-14 08:10:28 -0800

    Changed site title. Turned  offline plugin ON.

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   siteMetadata: {
-    title: `Leo Torres@BobaBird`,
+    title: `Leo Torres`,
     description: `My personal site.`,
     author: `Leo Torres <leohtor@gmail.com>`,
   },
@@ -30,6 +30,6 @@ module.exports = {
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.app/offline
-    // 'gatsby-plugin-offline',
+    'gatsby-plugin-offline',
   ],
 }
