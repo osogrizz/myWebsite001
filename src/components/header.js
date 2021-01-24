2547d484884cb8c4cc151f6ffa9b9551c6eb34f6
osogrizz <leo.htor@gmail.com>
2021-01-24 00:18:06 -0800

    Testing webkit flag to improve browser coverage.

--- src/components/header.js
+++ src/components/header.js
@@ -92,9 +92,9 @@ const LogoContainer = styled.div`
 
   }
   @media screen and (max-width: 500px) {
-    /* height: 100%; */
    .gatsby-image-wrapper  {
-     height: 100%;
+     filter: drop-shadow(2px 4px 6px black);
+    --webkit-filter: drop-shadow(2px 4px 6px black);
      }
   }
 
